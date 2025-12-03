a1cf56f850901ec5e0611dfd87e398f69961487d
uug4na <uug4na@gmail.com>
2025-12-03 15:22:25 +0800

    dark theme fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ export default {
   ],
   darkMode: 'class', // Enable dark mode with class strategy
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'comic-green': '#50C878',
+        'comic-yellow': '#F4D03F',
+        'comic-red': '#E74C3C',
+        'comic-blue': '#3498DB',
+        'comic-purple': '#9B59B6',
+        'paper-white': '#F9F7F2',
+        'ink-black': '#121212',
+        'dark-paper': '#1e1e1e',
+      },
+      fontFamily: {
+        'display': ['Bangers', 'cursive'],
+        'comic': ['Comic Neue', 'cursive'],
+        'body': ['Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        'comic': '4px 4px 0px 0px rgba(0,0,0,1)',
+        'comic-lg': '8px 8px 0px 0px rgba(0,0,0,1)',
+        'comic-dark': '4px 4px 0px 0px rgba(255,255,255,0.2)',
+      },
+    },
   },
   plugins: [],
 }
